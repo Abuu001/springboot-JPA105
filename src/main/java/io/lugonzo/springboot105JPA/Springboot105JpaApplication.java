d0c68474ec50fa3520cd5f4c5baa142357c44fa4
Abuu001 <abulugz@gmail.com>
2021-09-15 12:37:25 +0300

    first commit

---
+++ src/main/java/io/lugonzo/springboot105JPA/Springboot105JpaApplication.java
@@ -0,0 +1,13 @@
+package io.lugonzo.springboot105JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot105JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot105JpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/lugonzo/springboot105JPA/Springboot105JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lugonzo.springboot105JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot105JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
