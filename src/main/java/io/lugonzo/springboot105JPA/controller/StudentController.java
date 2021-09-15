068bde85ac577cc851ffa7fe0ee3028b44a0d139
Abuu001 <abulugz@gmail.com>
2021-09-15 23:53:30 +0300

    student controller

--- src/main/java/io/lugonzo/springboot105JPA/controller/StudentController.java
+++ src/main/java/io/lugonzo/springboot105JPA/controller/StudentController.java
@@ -1,2 +1,10 @@
-package io.lugonzo.springboot105JPA.controller;public class StudentController {
+package io.lugonzo.springboot105JPA.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class StudentController {
+
 }
