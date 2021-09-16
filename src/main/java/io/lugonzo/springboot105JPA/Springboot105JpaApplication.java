c400c51428a5c1ea7582602a34cc485494b8e22f
Abuu001 <abulugz@gmail.com>
2021-09-16 21:54:21 +0300

    md

--- src/main/java/io/lugonzo/springboot105JPA/Springboot105JpaApplication.java
+++ src/main/java/io/lugonzo/springboot105JPA/Springboot105JpaApplication.java
@@ -2,6 +2,7 @@ package io.lugonzo.springboot105JPA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class Springboot105JpaApplication {
