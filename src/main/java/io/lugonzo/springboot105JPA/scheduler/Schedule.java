d7b164c199f66e8a31cd7b6847ef931b8ca37a29
Abuu001 <abulugz@gmail.com>
2021-09-16 21:54:10 +0300

    task scheduler

--- src/main/java/io/lugonzo/springboot105JPA/scheduler/Schedule.java
+++ src/main/java/io/lugonzo/springboot105JPA/scheduler/Schedule.java
@@ -1,2 +1,24 @@
-package io.lugonzo.springboot105JPA.scheduler;public class Schedule {
+package io.lugonzo.springboot105JPA.scheduler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+@Slf4j
+@EnableScheduling
+public class Schedule {
+
+    @Scheduled(fixedRate = 2000L ,initialDelay = 5000L)
+    public void job1(){
+        log.info("Job1 current Time " + new Date());
+    }
+
+    @Scheduled(fixedRate = 2000L ,initialDelay = 5000L)
+    public void job2(){
+        log.info("Job2 current Time " + new Date());
+    }
 }
